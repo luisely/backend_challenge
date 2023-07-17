93a2d70b69810b842f512d3bc0ae4896acd5f867
Fernando Ely <luisely@gmail.com>
2023-07-17 14:01:10 -0300

    removido importacao que era nao utilizada

--- src/depoimentos/depoimentos-home.controller.ts
+++ src/depoimentos/depoimentos-home.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { DepoimentosService } from './depoimentos.service';
 
 @Controller('depoimentos-home')
