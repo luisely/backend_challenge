7b4d90c1c23311234839059bc96f0927fb44ae66
Fernando Ely <luisely@gmail.com>
2023-07-17 12:42:27 -0300

    habilitado pasta publica images

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './shared/database/database.module';
 import { DepoimentosModule } from './depoimentos/depoimentos.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [DatabaseModule, DepoimentosModule],
+  imports: [
+    DatabaseModule,
+    DepoimentosModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'tmp/images'),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
